69773f01183cf16e93d36f1a8f233d7373e476c6
BeomChi <changmin000705@gmail.com>
2024-10-19 01:48:39 +0900

    업로드
    1학년때 제작한 게임 빌드 파일은 있으나 프로젝트 파일을 못찾아 찾는 데로 업로드

---
+++ 만든 게임/2학년/PetPetRabbit(20.08.30~21.04)/Assets/Scripts/Stage/WinDefeatReferee.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Rabbit
+{
+    public class WinDefeatReferee : MonoBehaviour
+    {
+        [SerializeField] private NoteHandler noteHandler;
+        
+        [SerializeField] private GameTime gameTime;
+        [SerializeField] private GameHealth gameHealth;
+
+        [Header("승리할 시 Enable 될 UI Object")]
+        [SerializeField] private GameObject gameWinUI;
+        [Header("패배할 시 Enable 될 UI Object")]
+        [SerializeField] private GameObject gameDefeatUI;
+
+        private Coroutine winJudgeRoutine;
+        private Coroutine defeatJudgeRoutine;
+
+        private void Start()
+        {
+            winJudgeRoutine = StartCoroutine(WinJudge());
+            defeatJudgeRoutine = StartCoroutine(DefeatJudge());
+        }
+
+        private IEnumerator WinJudge()
+        {
+            yield return new WaitUntil(() => 
+                !noteHandler.IsNoteExist && StageManager.GameState == StageState.Start);
+            EndJudgeProcess();
+            
+            StopCoroutine(defeatJudgeRoutine);
+
+            if (StageManager.CurrentLevel == StageManager.MaxLevelIndex) {
+                // TODO: SceneFadeOut
+                SceneLoader.LoadScene("CreditScene");
+            }
+            else {
+                gameWinUI.SetActive(true);   
+            }
+        }
+
+        private IEnumerator DefeatJudge()
+        {
+            yield return new WaitUntil(() => 
+                (gameHealth.Health <= 0.0f || gameTime.IsTimeOver) && StageManager.GameState == StageState.Start);
+            EndJudgeProcess();
+
+            StopCoroutine(winJudgeRoutine);
+            
+            gameDefeatUI.SetActive(true);
+        }
+
+        private void EndJudgeProcess()
+        {
+            StageManager.SetStageState(StageState.End);
+            gameTime.StopTimer();
+        }
+    }
+}
---
+++ 만든 게임/2학년/PetPetRabbit(20.08.30~21.04)/Assets/Scripts/UI/UIStageInfo.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RotaryHeart.Lib.SerializableDictionary;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace Rabbit
+{
+    public class UIStageInfo : MonoBehaviour
+    {
+        [System.Serializable]
+        public class NoteUIInfo
+        {
+            public Sprite sprite;
+            public string key;
+        }
+
+        [System.Serializable]
+        public class NoteUIInfoSocket
+        {
+            public GameObject infoObject;
+            public Image imageSocket;
+            public TMP_Text textSocket;
+
+            public void SetInfo(Sprite sprite, string key)
+            {
+                imageSocket.sprite = sprite;
+                textSocket.text = key;
+            }
+        }
+        
+        [System.Serializable]
+        public class NoteInfoDictionary : SerializableDictionaryBase<NoteType, NoteUIInfo> { }
+
+        [SerializeField] private NoteInfoDictionary infoDic;
+        [SerializeField] private NoteUIInfoSocket[] infoSocket;
+
+        [SerializeField] private UnityEvent stageChangeEvent;
+
+        private void Start()
+        {
+            StartCoroutine(ShowStageNoteInfo());
+        }
+
+        private void Update()
+        {
+            if (StageManager.GameState != StageState.ShowInfo) return;
+
+            if (Keyboard.current.anyKey.wasPressedThisFrame) {
+                stageChangeEvent.Invoke();
+            }
+        }
+
+        private IEnumerator ShowStageNoteInfo()
+        {
+            yield return new WaitUntil(() => StageManager.GameState == StageState.ShowInfo);
+
+            SetStageInfo(StageManager.LevelData.infoShowTypes);
+        }
+
+        private void SetStageInfo(IReadOnlyList<NoteType> types)
+        {
+            foreach (var socket in infoSocket) {
+                socket.infoObject.SetActive(false);
+            }
+
+            for (var i = 0; i < types.Count; i++) {
+                infoSocket[i].infoObject.SetActive(true);
+                infoSocket[i].SetInfo(infoDic[types[i]].sprite, infoDic[types[i]].key);
+            }
+        }
+    }
+}
